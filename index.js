7b802802008a906d6abed5473de75834f78d0d4a
Akhetopnu <akhetopnu@protonmail.com>
2019-03-21 20:43:58 +0100

    asd

--- index.js
+++
@@ -1,7 +0,0 @@
-import * as array from './array';
-import * as channel from './channel';
-import * as dom from './dom';
-import * as object from './object';
-import * as string from './string';
-
-export { array, channel, dom, object, string };
